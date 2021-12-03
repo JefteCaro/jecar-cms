f3585d44d791db96a1ca820bc19001954d41ddac
Jecar <carojefte@gmail.com>
2021-12-04 05:12:49 +0800

    page fillable

--- src/Models/Page.php
+++ src/Models/Page.php
@@ -2,10 +2,23 @@
 
 namespace Jecar\Cms\Models;
 
-use Illuminate\Database\Eloquent\Model as BaseModel;
+use Jecar\Core\Models\Model;
+use Illuminate\Support\Str;
 
-class Page extends BaseModel
+class Page extends Model
 {
 
+    protected $fillable = [
+        'name',
+        'meta_title',
+        'meta_description',
+        'meta_keywords',
+        'meta_image',
+        'published',
+    ];
+
+    protected $casts = [
+        'published' => 'boolean',
+    ];
 
 }
